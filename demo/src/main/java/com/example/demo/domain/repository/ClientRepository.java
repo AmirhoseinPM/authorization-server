602fc56e15f37da106972fbd9e24dc30b7d80590
Amirhosein PooyanMoghadam <103578053+AmirhoseinPM@users.noreply.github.com>
2025-04-03 04:28:26 +0330

    import first

---
+++ demo/src/main/java/com/example/demo/domain/repository/ClientRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.domain.repository;
+import java.util.Optional;
+
+
+import com.example.demo.domain.entity.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, String> {
+    Optional<Client> findByClientId(String clientId);
+}
